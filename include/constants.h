afcd397616793d7b026786a59ad330eb16b3b493
Nyaaboron <jmarlpineda21@gmail.com>
2025-07-11 15:06:11 -0400

    Changed settings to make it feel more like the original flappy bird

--- include/constants.h
+++ include/constants.h
@@ -5,12 +5,12 @@
 
 #define SCREEN_WIDTH	288
 #define SCREEN_HEIGHT	512
-#define GRAVITY		2000.0f
-#define JUMP_FORCE	500.0f
-#define SCROLL_SPEED	130.0f
-#define PIPE_SPREAD	180.0f
+#define GRAVITY		1800.0f
+#define JUMP_FORCE	420.0f
+#define SCROLL_SPEED	80.0f
+#define PIPE_SPREAD	140.0f
 // EASY: 70, MEDIUM: 60, HARD: 50, EXTREME: 45
-#define PIPE_GAP	60.0f
+#define PIPE_GAP	50.0f
 #define GROUND_HEIGHT	50.0f
 #define TRANSITION_TIME	1.0f
 
